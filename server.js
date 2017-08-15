2036f50423bc29fd37a3faa95e165f0462aeae42
caseymacpowell <caseymac.powell@gmail.com>
2017-08-14 18:32:22 -0600

    Initial commit. Added main files and created server.

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongod = require('mongodb');
+var ObjectID  = require('mongodb').ObjectId;
+
+var db;
+
+mongodb.MongoClient.connect('mongob://localhost', function(err, database){
+	if (err) {
+		console.log(err);
+		return;
+	}
+	console.log("Connected to Database.");
+	db = database;
+	startListening();
+});
+
+var app = express();
+
+app.use(bodyparser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+//Files to be served 
+app.use(express.static('./'));
+
+// 404 boilderplate
+app.use(function(req, res, next){
+	res.status(404);
+	res.send("File Not Found!");
+});
+
+// 500 boilderplate
+app.use(function(err, req, res, next){
+	console.log(err);
+	res.status(500);
+	res.send("Internal Server Error!");
+	res.send(err);
+});
+
+function startListening(){
+	app.listen(3000, function(){
+		console.log("Server started at http://localhost:3000");
+	});
+}
